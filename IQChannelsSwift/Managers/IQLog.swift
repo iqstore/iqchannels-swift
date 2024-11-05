f253aa78032e119a91ebf35438fc56615f269acf
Mikhail <zinkov04@bk.ru>
2024-11-05 15:09:47 +0700

    SDK-88: Test

---
+++ IQChannelsSwift/Managers/IQLog.swift
@@ -0,0 +1,29 @@
+//
+//  IQLog.swift
+//  IQChannelsSwift
+//
+//  Created by Mikhail Zinkov on 02.11.2024.
+//
+
+import SwiftUI
+import OSLog
+
+class IQLog {
+    private static var isLoggingEnabled = true
+
+    static func configure(state: Bool) {
+        isLoggingEnabled = state
+    }
+
+    static func debug(message: String) {
+        if isLoggingEnabled {
+            os_log("%{public}s", type: .debug, "[DEBUG]: \(message)")
+        }
+    }
+
+    static func error(message: String) {
+        if isLoggingEnabled {
+            os_log("%{public}s", type: .error, "[ERROR]: \(message)")
+        }
+    }
+}
